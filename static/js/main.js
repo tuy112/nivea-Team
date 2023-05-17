ce00290dbc25a7ca2122773e9aee25c799fdae88
jangdu <jjd0324@gmail.com>
2023-05-17 10:52:58 +0900

    feat: detail페이지

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,13 @@
 const onClickSendEmailBtn = () => {
   location.href = "/sendEmail";
 };
+function onClick() {
+  $.ajax({
+    data: {
+      query: $('input[name="query"]').val(),
+      page: page,
+    },
+    type: "GET",
+    url: "/list",
+  });
+}
