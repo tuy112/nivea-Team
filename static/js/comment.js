969085f021d9287f332c0c82c1a6f39c81fb6d09
KimJaeHyeok <master_catch@naver.com>
2023-05-18 10:51:15 +0900

    2023.05.18 (JH)

--- static/js/comment.js
+++ static/js/comment.js
@@ -1,6 +1,15 @@
 $(document).ready(function () {
+  set_temp();
   show_comment();
 });
+function set_temp() {
+  fetch("http://spartacodingclub.shop/sparta_api/weather/seoul")
+    .then((res) => res.json())
+    .then((data) => {
+      let temp = data["temp"];
+      $("#temp").text(temp);
+    });
+}
 function save_comment() {
   let name = $("#name").val();
   let comment = $("#comment").val();
@@ -19,30 +28,52 @@ function save_comment() {
     });
 }
 function show_comment() {
-  fetch("/guestbook")
-    .then((res) => res.json())
-    .then((data) => {
-      let rows = data["result"];
+  $.ajax({
+    type: "GET",
+    url: "/guestbook",
+    data: {},
+    success: function (response) {
+      // app.py #보여주기의 key 값인 names 를 함수 변수 response에 넣어주고 rows에 넣어줌
+      let rows = response["result"];
       $("#comment-list").empty();
-      rows.forEach((a) => {
-        let group = a["group"];
-        let name = a["name"];
-        let comment = a["comment"];
+      console.log(rows);
+      // i를 0으로 지정 rows.length = rows 의 길이만큼돌리고, i++ = i에 1추가, for = 이걸 반복
+      for (let i = 0; i < rows.length; i++) {
+        let name = rows[i]["name"];
+        let id = rows[i]["_id"];
+        let comment = rows[i]["comment"];
+        let group = rows[i]["group"];
+        // console.log(rows)
+        // ${name} = rows[i]['name'] , ${id} = rows[i]['_id']
         let temp_html = ` <div class="card">
-        <div class="card-body">
-          <blockquote class="blockquote mb-0">
-            <p>${comment}</p>
-            <footer class="blockquote-footer">${group}조 ${name}</footer>
+  <div class="card-body">
+    <blockquote class="blockquote mb-0">
+      <p>${comment}</p>
+      <footer class="blockquote-footer">${group}조 ${name}</footer>
 
-          </blockquote>
-          <div class="delete_button_box">
-              <button id="delete_button" type="button" class="btn btn-danger">
-                삭제
-              </button>
-            </div>
-        </div>
-      </div>`;
+    </blockquote>
+    <div class="delete_button_box">
+        <button onclick="delete_list(${id})" id="delete_button" type="button" class="btn btn-danger">
+          삭제
+        </button>
+      </div>
+  </div>
+</div>`;
+
         $("#comment-list").append(temp_html);
-      });
-    });
+      }
+    },
+  });
+}
+function delete_list(id) {
+  $.ajax({
+    type: "POST",
+    url: "/guestbook/delete",
+    data: { num_give: id }, // id라는 매개변수를 num_give라는 변수에 넣어줌 , num_give 는 다른곳에서 사용 가능.
+    success: function (response) {
+      // 성공하면 response에 넣어줌.
+      alert(response["msg"]);
+      window.location.reload();
+    },
+  });
 }
