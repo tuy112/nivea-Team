62edb176f899126f0f226915f0bd4f7bf7af7b4d
jangdu <jjd0324@gmail.com>
2023-05-19 20:24:38 +0900

    v_1.22

--- static/js/application.js
+++ static/js/application.js
@@ -1,63 +1,70 @@
 document.querySelector("#login-form").addEventListener("submit", function (e) {
-    if (document.querySelector("#name").value == "") {
-      // id 라는 id 를 선택하고 해당 input이 공백일 경우
-      e.preventDefault(); // 폼 전송을 막음
-      alert("⛔ 이름을 입력해주세요"); // '아이디를 입력해주세요' 라는 경고창을 띄움
-    } else if (document.querySelector("#email").value == "") {
-      // password 라는 id 를 선택하고 해당 input이 공백일 경우
-      e.preventDefault(); // 폼 전송을 막음
-      alert("⛔ 메일을 입력해주세요!"); // '비밀번호를 입력해주세요' 라는 경고창을 띄움
-    } else if (document.querySelector("#github").value == "") {
-      // password 라는 id 를 선택하고 해당 input이 공백일 경우
-      e.preventDefault(); // 폼 전송을 막음
-      alert("⛔ github 주소를 입력해주세요!"); // '비밀번호를 입력해주세요' 라는 경고창을 띄움
-    } else if (document.querySelector("#str").value == "") {
-      // password 라는 id 를 선택하고 해당 input이 공백일 경우
-      e.preventDefault(); // 폼 전송을 막음
-      alert("⛔ 장점을 입력해주세요!"); // '비밀번호를 입력해주세요' 라는 경고창을 띄움
-    } 
-    else {
-        const formData = new FormData(e.target.value);
-        console.log(formData);
-        formData.append("service_id", "service_s6l8dom");
-        formData.append("template_id", "template_qucfqur");
-        formData.append("user_id", "FhBDdklrwtHsn51F2");
-        let params = {
-            user_id: "FhBDdklrwtHsn51F2",
-            service_id: "service_s6l8dom",
-            template_id: "template_qucfqur",
-            template_params: {
-            name: document.querySelector('input[id="name"]').value,
-            email: document.querySelector('input[id="email"]').value,
-            url: document.querySelector('input[id="github"]').value,
-            str: document.querySelector('input[id="str"]').value,
-            },
-        };
-    
-        let headers = {
-            "Content-type": "application/json",
-        };
-    
-        let options = {
-            method: "POST",
-            headers: headers,
-            body: JSON.stringify(params),
-        };
-    
-        fetch("https://api.emailjs.com/api/v1.0/email/send", options)
-            .then((httpResponse) => {
-            if (httpResponse.ok) {
-                const admit = `   제출 완료🚨
-                승인을 기다리세요···  `;
-                alert(admit);
-            } else {
-                return httpResponse.text().then((text) => Promise.reject(text));
-            }
-            })
-            .catch((error) => {
-            console.log("Oops... " + error);
-            });
-    }
-    
-  });
-  
+  if (document.querySelector("#name").value == "") {
+    // id 라는 id 를 선택하고 해당 input이 공백일 경우
+    e.preventDefault(); // 폼 전송을 막음
+    alert("⛔ 이름을 입력해주세요"); // '아이디를 입력해주세요' 라는 경고창을 띄움
+  } else if (document.querySelector("#email").value == "") {
+    // password 라는 id 를 선택하고 해당 input이 공백일 경우
+    e.preventDefault(); // 폼 전송을 막음
+    alert("⛔ 메일을 입력해주세요"); // '비밀번호를 입력해주세요' 라는 경고창을 띄움
+  } else if (document.querySelector("#github").value == "") {
+    // password 라는 id 를 선택하고 해당 input이 공백일 경우
+    e.preventDefault(); // 폼 전송을 막음
+    alert("⛔ github 주소를 입력해주세요"); // '비밀번호를 입력해주세요' 라는 경고창을 띄움
+  } else if (document.querySelector("#str").value == "") {
+    // password 라는 id 를 선택하고 해당 input이 공백일 경우
+    e.preventDefault(); // 폼 전송을 막음
+    alert("⛔ 장점을 입력해주세요"); // '비밀번호를 입력해주세요' 라는 경고창을 띄움
+  } else {
+    e.preventDefault();
+    const formData = new FormData(e.target.value);
+    console.log(formData);
+    formData.append("service_id", "service_s6l8dom");
+    formData.append("template_id", "template_qucfqur");
+    formData.append("user_id", "FhBDdklrwtHsn51F2");
+    let params = {
+      user_id: "FhBDdklrwtHsn51F2",
+      service_id: "service_s6l8dom",
+      template_id: "template_qucfqur",
+      template_params: {
+        name: document.querySelector('input[id="name"]').value,
+        email: document.querySelector('input[id="email"]').value,
+        url: document.querySelector('input[id="github"]').value,
+        str: document.querySelector('input[id="str"]').value,
+      },
+    };
+
+    let headers = {
+      "Content-type": "application/json",
+    };
+
+    let options = {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(params),
+    };
+
+    fetch("https://api.emailjs.com/api/v1.0/email/send", options)
+      .then((httpResponse) => {
+        if (httpResponse.ok) {
+          console.log("aaaa");
+
+          const admit = `제출 완료 승인을 기다리세요··`;
+          alert(admit);
+          window.location.reload();
+        } else {
+          return httpResponse.text().then((text) => Promise.reject(text));
+        }
+      })
+      .catch((error) => {
+        console.log("Oops... " + error);
+      });
+
+    // .then((res) => {})
+    // .then((data) => {
+    //   const admit = `   제출 완료🚨
+    //   승인을 기다리세요···  `;
+    //   alert(admit);
+    // });
+  }
+});
--- static/js/comment.js
+++ static/js/comment.js
@@ -27,6 +27,7 @@ function save_comment() {
       window.location.reload();
     });
 }
+
 function show_comment() {
   $.ajax({
     type: "GET",
@@ -36,9 +37,12 @@ function show_comment() {
       // app.py #보여주기의 key 값인 names 를 함수 변수 response에 넣어주고 rows에 넣어줌
       let rows = response["result"];
       $("#comment-list").empty();
-      console.log(rows);
+
+      // [{name : 두혁, id: 123, 13살}, {병일, 3살}]
       // i를 0으로 지정 rows.length = rows 의 길이만큼돌리고, i++ = i에 1추가, for = 이걸 반복
-      for (let i = 0; i < rows.length; i++) {
+      //[{0} {1} {2} {3}]
+      //for (let i = 0; i < rows.length; i++) {     //
+      for (let i = rows.length - 1; i >= 0; i--) {
         let name = rows[i]["name"];
         let id = rows[i]["_id"];
         let comment = rows[i]["comment"];
@@ -49,13 +53,16 @@ function show_comment() {
         <div class="mycard" >
           <div class="my-card-body">
             <div class="blockquote mb-0">
-              <p>${comment}</p>
+              <p id=${comment}>${comment}</p>
               <footer class="blockquote-footer">${group}조 ${name}</footer>
 
             </div>
-            <div class="delete_button_box">
+            <div class="delete_button_box" id='${id}'>
                 <button onclick="delete_list(${id})" id="delete_button" type="button" class="btn btn-danger">
                   삭제
+                </button>
+                <button onclick="onclickUpdateBtn(${id})" id="delete_button" type="button" class="btn btn-danger">
+                  수정
                 </button>
               </div>
           </div>
@@ -71,6 +78,38 @@ function delete_list(id) {
     type: "POST",
     url: "/guestbook/delete",
     data: { num_give: id }, // id라는 매개변수를 num_give라는 변수에 넣어줌 , num_give 는 다른곳에서 사용 가능.
+    success: function (response) {
+      // 성공하면 response에 넣어줌.
+      alert(response["msg"]);
+      window.location.reload();
+    },
+  });
+}
+
+const test = (id) => {
+  console.log($(`#update-input`).val());
+};
+const onclickUpdateBtn = (id) => {
+  let temp_html = `
+    <input id='update-input' value=''/>
+      <button onclick="update_list(${id}, )" id="delete_button" type="button" class="btn btn-danger">
+      수정
+    </button>
+
+  `;
+  $(`#${id}`).append(temp_html);
+};
+
+function update_list(id) {
+  comment = $(`#update-input`).val();
+
+  $.ajax({
+    type: "UPDATE",
+    url: "/guestbook/update",
+    data: {
+      num_give: id,
+      comment_give: comment,
+    }, // id라는 매개변수를 num_give라는 변수에 넣어줌 , num_give 는 다른곳에서 사용 가능.
     success: function (response) {
       // 성공하면 response에 넣어줌.
       alert(response["msg"]);
